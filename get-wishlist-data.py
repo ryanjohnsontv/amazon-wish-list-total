035fb91317835ef1ee72ba30fd3bb68962f2d80b
Ryan Johnson <43426700+ryanjohnsontv@users.noreply.github.com>
2020-01-31 16:18:23 -0600

    Update get-wishlist-data.py

--- get-wishlist-data.py
+++ get-wishlist-data.py
@@ -1,24 +1,24 @@
-import urllib2
+import urllib
 from bs4 import BeautifulSoup
 
 # Create / open a file called wishlist.txt which will be a CSVfile
 f = open("wishlist.txt", "w")
 
-url = raw_input('Enter the URL of your public Amazon Wish List: ')
+url = input('Enter the URL of your public Amazon Wish List: ')
 
 # Open amazon.com url
-print "Getting data for your wish list!"
-page = urllib2.urlopen(url)
+print ("Getting data for your wish list!")
+page = urllib.request.urlopen(url)
 
 # Get data from page
 soup = BeautifulSoup(page, "html.parser")
 #itemName = soup.findAll(attrs={"class":"a-link-normal a-declarative"})[0].get_text()
-itemPrice = soup.findAll(attrs={"class":"a-size-base a-color-price a-text-bold"})
+itemPrice = soup.findAll(attrs={"class":"a-offscreen"})
 for price in itemPrice:
-    print price.get_text()
+    print (price.get_text())
 
 # Write item name and price to file
-f.write(itemPrice)
+f.write(str(itemPrice))
 
 # Done getting data! Close file.
 f.close()
