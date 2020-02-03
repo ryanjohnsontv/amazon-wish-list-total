89b2f434dbfaafa0af1f58d2e5114c7251b7b068
Ryan Johnson <43426700+ryanjohnsontv@users.noreply.github.com>
2020-02-03 11:12:09 -0600

    Update get-wishlist-data.py

--- get-wishlist-data.py
+++ get-wishlist-data.py
@@ -3,8 +3,7 @@ from urllib.request import urlopen
 from re import sub
 from decimal import Decimal
 
-# Create / open a file called wishlist.txt which will be a CSVfile
-f = open("wishlist.txt", "w")
+sum = 0
 
 url = input('Enter the URL of your public Amazon Wish List: ')
 
@@ -14,19 +13,12 @@ page = urllib.request.urlopen(url)
 
 # Get data from page
 soup = BeautifulSoup(page, "html.parser")
-#itemName = soup.findAll(attrs={"class":"a-link-normal a-declarative"})[0].get_text()
 itemPrice = soup.findAll(attrs={"class":"a-offscreen"})
-sum=0
+
 for price in itemPrice:
     value = price.get_text()
     valueItem = Decimal(sub(r'[^\d.]', '', value))
     #print (valueItem)
     sum = sum + valueItem
 
-# Write item name and price to file
-f.write(str(itemPrice))
-
 print(sum)
-
-# Done getting data! Close file.
-f.close()
