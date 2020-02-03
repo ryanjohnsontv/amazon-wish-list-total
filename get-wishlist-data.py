b7d6dd4015f4eab345841534f42130ea90e24b0c
Ryan Johnson <43426700+ryanjohnsontv@users.noreply.github.com>
2020-02-03 11:20:20 -0600

    Update get-wishlist-data.py

--- get-wishlist-data.py
+++ get-wishlist-data.py
@@ -8,7 +8,6 @@ sum = 0
 url = input('Enter the URL of your public Amazon Wish List: ')
 
 # Open amazon.com url
-print ("Getting data for your wish list!")
 page = urllib.request.urlopen(url)
 
 # Get data from page
@@ -18,7 +17,6 @@ itemPrice = soup.findAll(attrs={"class":"a-offscreen"})
 for price in itemPrice:
     value = price.get_text()
     valueItem = Decimal(sub(r'[^\d.]', '', value))
-    #print (valueItem)
     sum = sum + valueItem
 
-print('${}'.format(sum))
+print('Wish List Total: ' + ('${}'.format(sum)))
